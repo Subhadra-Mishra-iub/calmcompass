39c3d2f1787fa23c49f9e01ba13d64db4d3ca214
Subhadra Mishra <subhadramishra1994@gmail.com>
2025-12-26 01:58:29 -0500

    Add test page to verify routing works

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem', fontSize: '2rem' }}>
+      <h1>✅ Test Page Works!</h1>
+      <p>If you can see this, the app is working.</p>
+    </div>
+  );
+}
+
